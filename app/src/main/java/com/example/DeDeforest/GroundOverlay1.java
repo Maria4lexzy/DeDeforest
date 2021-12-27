3d468c79f8a207eae09b63edbd7aceb32ac61474
Maria4lexzy <37138736+Maria4lexzy@users.noreply.github.com>
2021-12-27 02:13:46 +0100

    Update to map
    -Retrieve user's current location + map image overlay in same activity
    -set map image overlay to clickable
    -Added datepicker for eventually setting time range for image retrieval from sentinel2
    -Removed previous implementation of rotated map image overlay

--- app/src/main/java/com/example/DeDeforest/GroundOverlay1.java
+++ app/src/main/java/com/example/DeDeforest/GroundOverlay1.java
@@ -1,11 +1,18 @@
 package com.example.DeDeforest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.SeekBar;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -23,11 +30,17 @@ import java.util.List;
 import DeDeforest.R;
 
 public class GroundOverlay1 extends  AppCompatActivity
-        implements SeekBar.OnSeekBarChangeListener, OnMapReadyCallback,
+        implements
+        SeekBar.OnSeekBarChangeListener,
+        OnMapReadyCallback,
+        GoogleMap.OnMyLocationButtonClickListener,
+        ActivityCompat.OnRequestPermissionsResultCallback,
+        GoogleMap.OnMyLocationClickListener,
         GoogleMap.OnGroundOverlayClickListener {
     private static final int TRANSPARENCY_MAX = 100;
 
     private static final LatLng NEWARK = new LatLng(40.714086, -74.228697);
+    private LatLng centerLocation;
 
     private static final LatLng NEAR_NEWARK =
             new LatLng(NEWARK.latitude - 0.001, NEWARK.longitude - 0.025);
@@ -36,11 +49,12 @@ public class GroundOverlay1 extends  AppCompatActivity
 
     private GroundOverlay groundOverlay;
 
-    private GroundOverlay groundOverlayRotated;
 
     private SeekBar transparencyBar;
 
     private int currentEntry = 0;
+    private GoogleMap mMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,35 +72,34 @@ public class GroundOverlay1 extends  AppCompatActivity
 
     @Override
     public void onMapReady(GoogleMap map) {
-        // Register a listener to respond to clicks on GroundOverlays.
-        map.setOnGroundOverlayClickListener(this);
-
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(NEWARK, 11));
-
-        images.clear();
-        images.add(BitmapDescriptorFactory.fromResource(R.drawable.newark_nj_1922));
-        images.add(BitmapDescriptorFactory.fromResource(R.drawable.newark_prudential_sunny));
-
-        // Add a small, rotated overlay that is clickable by default
-        // (set by the initial state of the checkbox.)
-        groundOverlayRotated = map.addGroundOverlay(new GroundOverlayOptions()
-                .image(images.get(1)).anchor(0, 1)
-                .position(NEAR_NEWARK, 4300f, 3025f)
-                .bearing(30)
-                .clickable(((CheckBox) findViewById(R.id.toggleClickability)).isChecked()));
-
-        // Add a large overlay at Newark on top of the smaller overlay.
-        groundOverlay = map.addGroundOverlay(new GroundOverlayOptions()
-                .image(images.get(currentEntry)).anchor(0, 1)
-                .position(NEWARK, 8600f, 6500f));
-
-        transparencyBar.setOnSeekBarChangeListener(this);
+        mMap=map;
+        mMap.setOnMyLocationButtonClickListener(this);
+        mMap.setOnMyLocationClickListener(this);
+        setOverlayImage(mMap);
+        enableMyLocation();
 
         // Override the default content description on the view, for accessibility mode.
         // Ideally this string would be localised.
         map.setContentDescription("Google Map with ground overlay.");
     }
+public void setOverlayImage(GoogleMap mMap){
+    // Register a listener to respond to clicks on GroundOverlays.
+    mMap.setOnGroundOverlayClickListener(this);
 
+    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(NEWARK, 11));
+
+    images.clear();
+    images.add(BitmapDescriptorFactory.fromResource(R.drawable.newark_nj_1922));
+    images.add(BitmapDescriptorFactory.fromResource(R.drawable.newark_prudential_sunny));
+
+    // Add a large overlay at Newark on top of the smaller overlay.
+    groundOverlay = mMap.addGroundOverlay(new GroundOverlayOptions()
+            .image(images.get(currentEntry)).anchor(0, 1)
+            .position(NEWARK, 8600f, 6500f)
+            .bearing(0)
+            .clickable(true));
+    transparencyBar.setOnSeekBarChangeListener(this);
+}
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
     }
@@ -113,17 +126,31 @@ public class GroundOverlay1 extends  AppCompatActivity
     @Override
     public void onGroundOverlayClick(GroundOverlay groundOverlay) {
         // Toggle transparency value between 0.0f and 0.5f. Initial default value is 0.0f.
-        groundOverlayRotated.setTransparency(0.5f - groundOverlayRotated.getTransparency());
+        groundOverlay.setTransparency(0.5f - groundOverlay.getTransparency());
     }
-
-    /**
-     * Toggles the clickability of the smaller, rotated overlay based on the state of the View that
-     * triggered this call.
-     * This callback is defined on the CheckBox in the layout for this Activity.
-     */
-    public void toggleClickability(View view) {
-        if (groundOverlayRotated != null) {
-            groundOverlayRotated.setClickable(((CheckBox) view).isChecked());
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            if (mMap != null) {
+                mMap.setMyLocationEnabled(true);
+            }
+        } else {
+            String myPermission[]={"android.permission.ACCESS_FINE_LOCATION"};
+            // Permission to access the location is missing. Show rationale and request permission
+            ActivityCompat.requestPermissions(this,myPermission, 200);
         }
     }
-}
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
+    }
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+        Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
+    }
+
+
+    }
