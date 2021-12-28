bfa1072b713be4425d9825f53a434006957dd98d
Maria4lexzy <37138736+Maria4lexzy@users.noreply.github.com>
2021-12-28 16:12:51 +0100

    Date Picker layout + implementation
    -Added datepicker to dialog + saving selected date

--- app/src/main/java/com/example/DeDeforest/RequestDataFragment.java
+++ app/src/main/java/com/example/DeDeforest/RequestDataFragment.java
@@ -1,9 +1,13 @@
 package com.example.DeDeforest;
 
+import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.DatePicker;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -11,23 +15,36 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 
+import java.util.Calendar;
+
+import DeDeforest.R;
+
 public class RequestDataFragment extends DialogFragment {
+    final Calendar newCalender=Calendar.getInstance();
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         super.onCreateDialog(savedInstanceState);
         String stringArgs=getArguments().getString("stringArgKey");
-        Toast.makeText(getContext(), "bounds:\n"+stringArgs, Toast.LENGTH_LONG).show();
 
         Log.i("ARGS", stringArgs);
 
         AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater=this.getLayoutInflater();
+        View view=inflater.inflate(R.layout.dialog_get_bounds,null);
         builder.setTitle("Select Date To Request Image")
+                .setView(view)
                 .setMessage(""+stringArgs)
                 .setPositiveButton("Send", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         //action to happen on click
+                        DatePicker datePicker=view.findViewById(R.id.datePicker1);
+                        int day=datePicker.getDayOfMonth();
+                        int month=datePicker.getMonth()+1;
+                        int year=datePicker.getYear();
+                        Toast.makeText(getContext(), "Date:\n"+day+" - "+month+" - "+year, Toast.LENGTH_LONG).show();
+
                     }
                 })
                 .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
